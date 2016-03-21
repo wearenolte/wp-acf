9ee810452bbf9c3e73ba2c850a113dde9aa9c8d3
adam <adam@solnamic.com>
2016-03-20 21:27:26 -0600

    Add transforms function plus PostObject transform.

---
+++ src/Transforms/PostObject.php
@@ -0,0 +1,43 @@
+<?php namespace Leean\Acf\Transforms;
+
+use Leean\Acf\All;
+
+/**
+ * Class PostObject.
+ *
+ * @package Leean\Acf\Transforms
+ */
+class PostObject
+{
+	/**
+	 * Apply the transforms
+	 *
+	 * @param array $field The field.
+	 * @return array
+	 */
+	public static function apply( $field ) {
+		if ( ! ( 'post_object' === $field['type'] && 'id' === $field['return_format'] ) ) {
+			return $field['value'];
+		}
+
+		return self::get_sub_post_fields( $field );
+	}
+
+	/**
+	 * Transform to get the WP and ACF fields of the sub post.
+	 *
+	 * @param array $field The field.
+	 * @return array
+	 */
+	public static function get_sub_post_fields( $field ) {
+		if ( is_array( $field['value'] ) ) {
+			$data = [];
+			foreach ( $field['value'] as $post_id ) {
+				$data[] = All::get_post_fields( $post_id );
+			}
+			return $data;
+		} else {
+			return All::get_post_fields( $field['value'] );
+		}
+	}
+}
